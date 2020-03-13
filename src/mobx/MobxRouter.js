c81bfbcfaf5a6c6228306a29cc1bcdf65a6d5beb
Kevin Purnelle <kevin.purnelle@gmail.com>
2020-03-13 14:53:28 +0100

    chore(router): fix export to have correct type

--- src/mobx/MobxRouter.js
+++ src/mobx/MobxRouter.js
@@ -2,10 +2,12 @@ import * as mobx from 'mobx'
 import Router from '../Router'
 const {observable, action, decorate} = mobx
 
-export default decorate(Router, {
+decorate(Router, {
   route: observable,
   params: observable.ref,
   story: observable.ref,
   goTo: action.bound,
   set: action.bound,
 })
+
+export default Router
